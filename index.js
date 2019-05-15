6f3cab4861867d851219eacc4f047b361542b20c
Andre Mueller <hello@andremueller.me>
2019-05-15 11:30:38 +0100

    Make port configurable via env variable PORT

--- index.js
+++ index.js
@@ -4,8 +4,10 @@ const server = jsonServer.create()
 const router = jsonServer.router('db.json')
 const middlewares = jsonServer.defaults()
 
+const port = process.env.PORT || 3000;
+
 server.use(middlewares)
 server.use(router)
-server.listen(3000, () => {
-  console.log('Aimless Hammer PoC SAPI Customer Accounts Balances is running')
+server.listen(port, () => {
+  console.log(`Aimless Hammer PoC SAPI Customer Accounts Balances is running on 'http://localhost:${port}'`)
 })
