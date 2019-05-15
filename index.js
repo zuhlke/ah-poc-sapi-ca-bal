afc7bc80368cd38e942f4dca107997378252f834
Andre Mueller <hello@andremueller.me>
2019-05-15 12:10:45 +0100

    Make resource read-only

--- index.js
+++ index.js
@@ -2,7 +2,9 @@
 const jsonServer = require('json-server')
 const server = jsonServer.create()
 const router = jsonServer.router('db.json')
-const middlewares = jsonServer.defaults()
+const middlewares = jsonServer.defaults({
+  'readOnly': true
+})
 
 const port = process.env.PORT || 3000;
 
