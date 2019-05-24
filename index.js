365d73c560ac0bd145454cc6a712c6bf196e90f2
Rik Howard <rik.howard@zuhlke.com>
2019-05-24 11:53:51 +0100

    Intro support for '/actuator/health' endpoint

--- index.js
+++ index.js
@@ -1,15 +1,22 @@
 // server.js
-const jsonServer = require('json-server')
-const server = jsonServer.create()
-const router = jsonServer.router('db.json')
+const jsonServer = require('json-server');
+const server = jsonServer.create();
+const router = jsonServer.router('db.json');
 const middlewares = jsonServer.defaults({
   'readOnly': true
-})
+});
 
 const port = process.env.PORT || 3000;
 
-server.use(middlewares)
-server.use(router)
+
+
+// Add custom routes before JSON Server router
+server.get('/actuator/health', (req, res) => {
+  res.jsonp([{'status':'UP'}])
+});
+
+server.use(middlewares);
+server.use(router);
 server.listen(port, () => {
   console.log(`Aimless Hammer PoC SAPI Customer Accounts Balances is running on 'http://localhost:${port}'`)
-})
+});
